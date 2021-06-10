0477327c6f4d48da4480b283214c00ed9e977ef5
Antonino Giuseppe Giano <tonygiano.tg@gmail.com>
2021-06-10 15:20:47 +0200

    view model Books Detail and Index

---
+++ Models/ViewModels/BookDetailViewModel.cs
@@ -0,0 +1,7 @@
+namespace Phrook.Models.ViewModels
+{
+    public class BookDetailViewModel : BookViewModel
+    {
+        public string Description { get; set; }
+    }
+}
---
+++ Models/ViewModels/BookViewModel.cs
@@ -0,0 +1,16 @@
+using Phrook.Models.Enums;
+
+namespace Phrook.Models.ViewModels
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+		public string ISBN { get; set; }
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public string ImagePath { get; set; }
+		public double Rating { get; set; }
+		public string Tag { get; set; }
+		public string ReadingState { get; set; }
+    }
+}
