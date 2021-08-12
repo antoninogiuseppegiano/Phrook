91e36e27263f0d5f57c54bd4bae63e903edf38f6
Antonino Giuseppe Giano <tony.giano@alecsandria.it>
2021-08-12 11:23:34 +0200

    Migrations

---
+++ Migrations/20210810131130_InitialMigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Phrook.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Isbn = table.Column<string>(type: "TEXT", nullable: true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    ImagePath = table.Column<string>(type: "TEXT", nullable: true),
+                    Author = table.Column<string>(type: "TEXT", nullable: true),
+                    Rating = table.Column<double>(type: "REAL", nullable: false),
+                    Tag = table.Column<string>(type: "TEXT", nullable: true),
+                    ReadingState = table.Column<string>(type: "TEXT", nullable: true),
+                    RowVersion = table.Column<string>(type: "TEXT", rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
---
+++ Migrations/20210810132851_TriggersBookVersion.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Phrook.Migrations
+{
+    public partial class TriggersBookVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			migrationBuilder.Sql(@"CREATE TRIGGER BooksSetRowVersionOnInsert
+                                   AFTER INSERT ON Books
+                                   BEGIN
+                                   UPDATE Books SET RowVersion = CURRENT_TIMESTAMP WHERE Id=NEW.Id;
+                                   END;");
+            migrationBuilder.Sql(@"CREATE TRIGGER BooksSetRowVersionOnUpdate
+                                   AFTER UPDATE ON Books WHEN NEW.RowVersion <= OLD.RowVersion
+                                   BEGIN
+                                   UPDATE Books SET RowVersion = CURRENT_TIMESTAMP WHERE Id=NEW.Id;
+                                   END;");
+            migrationBuilder.Sql("UPDATE Books SET RowVersion = CURRENT_TIMESTAMP;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+			migrationBuilder.Sql("DROP TRIGGER BooksSetRowVersionOnInsert;");
+            migrationBuilder.Sql("DROP TRIGGER BooksSetRowVersionOnUpdate;");
+        }
+    }
+}
